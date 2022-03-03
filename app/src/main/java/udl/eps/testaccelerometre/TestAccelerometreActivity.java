ccb7786f6331c13c61a3db88272c538b4b8ddda2
ahmedazzamouri <ahmedazzamouri98@gmail.com>
2022-03-03 12:51:50 +0100

    First commit

--- app/src/main/java/udl/eps/testaccelerometre/TestAccelerometreActivity.java
+++ app/src/main/java/udl/eps/testaccelerometre/TestAccelerometreActivity.java
@@ -3,81 +3,93 @@ package udl.eps.testaccelerometre;
 import android.app.Activity;
 import android.graphics.Color;
 import android.hardware.Sensor;
-import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 
+import android.widget.ScrollView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-public class TestAccelerometreActivity extends Activity implements SensorEventListener {
+
+import udl.eps.testaccelerometre.listeners.AccelerometerSensor;
+import udl.eps.testaccelerometre.listeners.LightSensor;
+
+public class TestAccelerometreActivity extends Activity {
   private SensorManager sensorManager;
-  private boolean color = false;
-  private TextView view;
-  private long lastUpdate;
+  private SensorEventListener lightListener;
+  private SensorEventListener accelerometerListener;
 
-  
-/** Called when the activity is first created. */
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
-    view = findViewById(R.id.textView);
-    view.setBackgroundColor(Color.GREEN);
 
+    TextView topView = findViewById(R.id.ViewTop);
+    ScrollView botView = findViewById(R.id.ViewBot);
+    TextView accStatus = findViewById(R.id.acceleratorSensorStatus);
+    TextView lightStatus = findViewById(R.id.LightSensorStatus);
+    topView.setBackgroundColor(Color.GREEN);
+    botView.setBackgroundColor(Color.YELLOW);
+
+    lightListener = new LightSensor(this);
+    accelerometerListener = new AccelerometerSensor(this);
     sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-    //if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
-      sensorManager.registerListener(this,
+
+    if (sensorManager != null && sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null){
+      accStatus.setText(getResources().getString(R.string.accelerometer_sensor_good));
+      showSensorData(sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER));
+      sensorManager.registerListener(accelerometerListener,
               sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
               SensorManager.SENSOR_DELAY_NORMAL);
-      // register this class as a listener for the accelerometer sensor
-    //}
-      lastUpdate = System.currentTimeMillis();
-  }
 
-  @Override
-  public void onSensorChanged(SensorEvent event) {
-          getAccelerometer(event);
-  }
+    } else {
+      accStatus.setText(getResources().getString(R.string.accelerometer_sensor_bad));
+    }
 
-  private void getAccelerometer(SensorEvent event) {
-    float[] values = event.values;
-    // Movement
-    float x = values[0];
-    float y = values[1];
-    float z = values[2];
+    if (sensorManager != null && sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT) != null){
+      lightStatus.setText(String.format(getString(R.string.light_sensor_good),
+              sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT).getMaximumRange()));
+      sensorManager.registerListener(lightListener,
+              sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT),
+              SensorManager.SENSOR_DELAY_NORMAL);
 
-    float accelationSquareRoot = (x * x + y * y + z * z)
-        / (SensorManager.GRAVITY_EARTH * SensorManager.GRAVITY_EARTH);
-    long actualTime = System.currentTimeMillis();
-    if (accelationSquareRoot >= 2) 
-    {
-      if (actualTime - lastUpdate < 200) {
-        return;
-      }
-      lastUpdate = actualTime;
-
-      Toast.makeText(this, R.string.shuffed, Toast.LENGTH_SHORT).show();
-      if (color) {
-        view.setBackgroundColor(Color.GREEN);
-      } else {
-        view.setBackgroundColor(Color.RED);
-      }
-      color = !color;
+    } else {
+      lightStatus.setText(R.string.light_sensor_bad);
     }
   }
 
-  @Override
-  public void onAccuracyChanged(Sensor sensor, int accuracy) {
-    // Do something here if sensor accuracy changes.
+  private void showSensorData(Sensor sensor){
+    showSensorData(sensor.getResolution(), sensor.getMaximumRange(), sensor.getPower());
+  }
+
+  private void showSensorData(float resolution, float range, float power){
+    TextView sensorData = findViewById(R.id.acceleratorSensorData);
+    sensorData.setText(String.format(getString(R.string.accelerometer_sensor_data),resolution, range, power));
+
   }
 
   @Override
   protected void onPause() {
     // unregister listener
     super.onPause();
-    sensorManager.unregisterListener(this);
+    sensorManager.unregisterListener(lightListener);
+    sensorManager.unregisterListener(accelerometerListener);
   }
-} 
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+    sensorManager.unregisterListener(lightListener);
+    sensorManager.unregisterListener(accelerometerListener);
+  }
+
+  @Override
+  protected void onRestart() {
+    super.onRestart();
+    if (sensorManager != null && sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT) != null)
+      sensorManager.registerListener(lightListener, sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT), SensorManager.SENSOR_DELAY_NORMAL);
+    if (sensorManager != null && sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null)
+      sensorManager.registerListener(accelerometerListener, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+  }
+}
